e0f72bea9061c0b53e445288f166608f1fc768f9
gallaghero <oceannegallagher@gmail.com>
2016-07-08 14:51:43 +0100

    Update BorwellApplication.java

--- BorwellApplication.java
+++ BorwellApplication.java
@@ -1,8 +1,4 @@
- /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package borwellapplication;
 
 
@@ -10,10 +6,8 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.*;
-/**
- *
- * @author Peni
- */
+
+
 public class BorwellApplication extends JFrame{
     
         //declare components of window
